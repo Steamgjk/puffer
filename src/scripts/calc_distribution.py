4523cfe71fc20c688e7721a38589f3e28bdd7132
耿金坤 <steam1994@163.com>
2020-04-07 00:55:42 -0700

    ok

--- src/scripts/calc_distribution.py
+++ src/scripts/calc_distribution.py
@@ -27,6 +27,28 @@ CLIENT_BUFFER_FILE_PREFIX = 'client_buffer_'
 FILE_SUFFIX = 'T11.csv'
 PKT_BYTES = 1500
 OUTPUT_STATS="output_stats"
+def calc_throughput_sub(video_sent_file_name, video_acked_file_name):
+    throughput_hist = {}
+    if path.exists(video_sent_file_name) and path.exists(video_acked_file_name):
+        video_sent_rows = read_csv_to_rows(video_sent_file_name)
+        video_acked_rows = read_csv_to_rows(video_acked_file_name)
+        print("len ", len(video_sent_rows), " ", len(video_acked_rows))
+        raw_data = process_raw_csv_data(video_sent_rows, video_acked_rows, None)
+        del video_sent_rows, video_acked_rows
+        gc.collect()
+        #print(raw_data)
+        for session in raw_data:
+            for chunk in raw_data[session]:
+                if 'trans_time' in raw_data[session][chunk]:
+                    transmission_time  = raw_data[session][chunk]['trans_time']
+                    chunk_size = raw_data[session][chunk]['size']
+                    throughput = chunk_size*1.0/transmission_time  # bytes/seconds
+                    throughput = int(round(throughput))
+                    if throughput not in throughput_hist:
+                        throughput_hist[throughput] = 0
+                    throughput_hist[throughput] += 1
+    return  throughput_hist
+
 def calc_throughput(folder_name, start_date):
     print("Geting ", folder_name, " start_date=",start_date)
     url_str = "https://storage.googleapis.com/puffer-stanford-data/"+folder_name+".tar.gz"
@@ -41,42 +63,32 @@ def calc_throughput(folder_name, start_date):
     throughput_hist = {}
     min_throuput = sys.maxsize
     max_throughput = 0
-    for i in range(0, 32):
-        date_item = start_date + timedelta(days=i)
-        video_sent_file_name = folder_name+"/"+ VIDEO_SENT_FILE_PREFIX + date_item.strftime('%Y-%m-%d') + FILE_SUFFIX
-        video_acked_file_name = folder_name+"/"+ VIDEO_ACKED_FILE_PREFIX + date_item.strftime('%Y-%m-%d') + FILE_SUFFIX
-        
-        if path.exists(video_sent_file_name) and path.exists(video_acked_file_name):
-            video_sent_rows = read_csv_to_rows(video_sent_file_name)
-            video_acked_rows = read_csv_to_rows(video_acked_file_name)
-            print("len ", len(video_sent_rows), " ", len(video_acked_rows))
-            raw_data = process_raw_csv_data(video_sent_rows, video_acked_rows, None)
-            del video_sent_rows, video_acked_rows
-            gc.collect()
-            #print(raw_data)
-            
-            for session in raw_data:
-                for chunk in raw_data[session]:
-                    if 'trans_time' in raw_data[session][chunk]:
-                        transmission_time  = raw_data[session][chunk]['trans_time']
-                        chunk_size = raw_data[session][chunk]['size']
-                        throughput = chunk_size*1.0/transmission_time  # bytes/seconds
-                        throughput = int(round(throughput))
-                        if throughput not in throughput_hist:
-                            throughput_hist[throughput] = 0
-                            if min_throuput > throughput:
-                                min_throuput = throughput
-                            if max_throughput < throughput:
-                                max_throughput = throughput
-                        throughput_hist[throughput] += 1
-        else:
-            continue 
+    pool = Pool(processes= 8)
     sorted_hist = {}
+    result = [] 
+    for j in range(4)   
+        for i in range(8*j, 8*j+8):
+            date_item = start_date + timedelta(days=i)
+            video_sent_file_name = folder_name+"/"+ VIDEO_SENT_FILE_PREFIX + date_item.strftime('%Y-%m-%d') + FILE_SUFFIX
+            video_acked_file_name = folder_name+"/"+ VIDEO_ACKED_FILE_PREFIX + date_item.strftime('%Y-%m-%d') + FILE_SUFFIX
+            result.append(pool.apply_async(calc_throughput, args=(video_sent_file_name, video_acked_file_name)))
+            for res in result:
+                hist = res.get() 
+                for throughput in hist:
+                    if min_throuput > throughput:
+                        min_throuput = throughput
+                    if max_throughput < throughput:
+                        max_throughput = throughput
+                    if throughput not in sorted_hist:
+                        throughput_hist[throughput] = 0
+                    throughput_hist[throughput] += hist[throughput]
+    
     for i in range(min_throuput, max_throughput+1):
         if  i in throughput_hist:
             sorted_hist[i] = throughput_hist[i]
     print(sorted_hist)
     print("FIN Calc")
+
     cmd = "rm -rf " + folder_name+".tar.gz"
     cmd = shlex.split(cmd)
     subprocess.call(cmd, shell=False)
@@ -113,6 +125,7 @@ def main():
     pool.close()
     pool.join()
     '''
+    '''
     for i in range(1,13):
         start_dt = datetime(year = 2019, month=i, day = 1)
         folder_name = "puffer-"+"2019"+ str(i).zfill(2)
@@ -125,7 +138,7 @@ def main():
     #start_dt = datetime(year = 2020, month=2, day = 1)
     #folder_name = "puffer-fake-sample"
     calc_throughput(folder_name, start_dt)
-    '''
+    
     return
 
 if __name__ == '__main__':
