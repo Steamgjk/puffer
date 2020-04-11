71b7d0530aceda324fb484610789b9a4cb9c59ec
耿金坤 <steam1994@163.com>
2020-04-11 00:35:42 -0700

    update  Y&#Y& V$

--- src/scripts/calc_distribution.py
+++ src/scripts/calc_distribution.py
@@ -12,14 +12,17 @@ from multiprocessing import Process, Array, Pool
 import subprocess
 import shlex
 import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import gc
 import json
+import tkinter
+import scipy.stats
+
 from ttp_local import(
     read_csv_to_rows,
     process_raw_csv_data
 )
-
 
 VIDEO_SENT_FILE_PREFIX = 'video_sent_'
 VIDEO_ACKED_FILE_PREFIX = 'video_acked_'
@@ -130,8 +133,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sta', dest='sta', type=int)  
     parser.add_argument('--end', dest='end', type=int)  
-    '''
     args = parser.parse_args()
+    '''
     cmd = "mkdir "+OUTPUT_STATS
     if path.exists(OUTPUT_STATS) is not True:
         cmd = shlex.split(cmd)
@@ -148,14 +151,102 @@ if __name__ == '__main__':
     #start_date = datetime(year = 2020, month=2, day = 1)
     #folder_name = "puffer-fake-sample"
     #calc_throughput(folder_name, start_date)
+    max_val = 0
+    min_val = sys.maxsize
+    json_arr = []
+    bin_cap = 5000
+    bin_size = 15
+    max_val = -1
+    row_size = 5
+    col_size = 3
+    
+    f, ax = plt.subplots(row_size, col_size, figsize=(10,15), sharex=False, sharey=False)
+    plt.subplots_adjust(wspace=0.2, hspace=0.2)
+    distris = []
+    lbs =[]
+    fig_cnt = 0
+    for y in range(2019, 2021):
+        for i in range(args.sta,args.end):
+            file_name = OUTPUT_STATS+"/"+"puffer-"+str(y)+ str(i).zfill(2)+".stat"
+            if path.exists(file_name) == False:
+                continue
+            f = open(file_name, "r")
+            jsonObj = json.load(f)
+            f.close()
+            invalid_keys = []
+            for key in jsonObj:
+                if int(key) < 0:
+                    invalid_keys.append(key)
+            for invalid_key in invalid_keys:
+                del jsonObj[invalid_key]
 
-    for i in range(args.sta,args.end):
-        file_name = OUTPUT_STATS+"/"+"puffer-"+"2019"+ str(i).zfill(2)+".stat"
-        f = open(file_name, "r")
-        jsonObj = json.load(f)
-        print(type(jsonObj))
-        for key in jsonObj:
-            print(key, " ", jsonObj[key])
+            distri = [0 for k in range(bin_size)]
+            ele_num = len(jsonObj)
+            for key in jsonObj:
+                key = int(key)
+                #print("  ", int(key/bin_cap), " ", (key/bin_cap), " ", key," ",bin_cap )
+                if int(key/bin_cap) < bin_size:
+                    distri[ int(key/bin_cap) ] += 1
+                else:
+                    distri[-1] += 1
+            for k in range(bin_size):
+                distri[k] = distri[k] * 100.0/ele_num
+            
+            distris.append(distri)
+            lbs.append(str(y)+ str(i).zfill(2))
+            sum_val = 0
+            for k in range(bin_size):
+                sum_val += distri[k]
+            print("sum_val=", sum_val)
+            print(distri)
+            xx =  [(k) for k in range(bin_size)]
+            xxx = [int(bin_cap*k/1000) for k in range(bin_size)]
+            row_idx = int(fig_cnt/col_size)
+            col_idx = int(fig_cnt%col_size)
+            
+            ax[row_idx][col_idx].set_ylim(0,12)
+            ax[row_idx][col_idx].set_xticks(xx, xxx)
+            ax[row_idx][col_idx].bar(xx, distri)
+            ax[row_idx][col_idx].set_xlabel("Throughput (1000 Packets/Second)")
+            ax[row_idx][col_idx].set_ylabel("Percentage (%)")
+            title = "Throughput Distribution "+ str(y)+ str(i).zfill(2)
+            ax[row_idx][col_idx].set_title(title)
+            '''
+            plt.ylim(0,12)
+            plt.xticks(xx, xxx)
+            plt.bar(xx, distri)
+            plt.xlabel("Throughput (1000 Packets/Second)")
+            plt.ylabel("Percentage (%)")
+            title = "Throughput Distribution "+ str(y)+ str(i).zfill(2)
+            plt.title(title)
+            plt.savefig(title+".png")
+            plt.close()
+            '''
+            fig_cnt += 1
+
+    plt.subplots_adjust(wspace=0.5, hspace=0.5)
+    plt.savefig("Distribution.png")
+
+    print("KL Divergence \n\n\n ")
+    distri_num = len(distris)
+    for i in range(distri_num):
+        for k in range(len(distris[i])):
+            if(distris[i][k] == 0):
+                distris[i][k] = 0.00001
+    kl_divergences = [ [0.0 for i in range(distri_num)] for j in range(distri_num)]
+    line = "\t"
+    for i in range(distri_num):
+        line += lbs[i]+"\t"
+    print(line)
+    for i in range(distri_num):
+        line = lbs[i] + "\t"
+        for j in range(distri_num):    
+            kl_divergences[i][j] = round(scipy.stats.entropy(distris[i], distris[j]),3) 
+            line += str(kl_divergences[i][j])+ "\t"
+        print(line)
+    
+            
+    
 
 
 
